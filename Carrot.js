417a4cf1449ef79f48e32ec53a4e6a31de62fa5a
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-09 07:43:24 +0700

    Update cr
    Update cr

--- Carrot.js
+++ Carrot.js
@@ -1,1 +1,61 @@
-alert("sdsd");
+class Carrot{
+
+    lang="en";
+    color_btn="#fa1675";
+
+    loadJs(path_js, obj_call, func_call = "show") {
+        if(window[obj_call]!=null){
+            window[obj_call][func_call]();
+        }else{
+            $.getScript(path_js).done(function(script, textStatus) {
+                if(obj_call!=null) window[obj_call][func_call]();
+            })
+            .fail(function(jqxhr, settings, exception) {
+                console.log("Script loading failed: " + exception);
+            });
+        }
+    }
+
+    set_color_btn(color){
+        cr.color_btn=color;
+    }
+
+    show_setting(act_done=null){
+        var html='';
+        html+='<form>';
+        html+='<div class="form-group">';
+            html+='<label for="exampleInputEmail1"><i class="fas fa-globe-asia"></i> Language</label>';
+            html+='<select class="form-control" id="dropdown_lang"><select>';
+            html+='<small id="emailHelp" class="form-text text-muted">Select your country and language</small>';
+        html+='</div>';
+        html+='</form>';
+        Swal.fire({
+            title:"Setting",
+            html:html,
+            showCancelButton: true,
+            showCloseButton: true,
+            confirmButtonColor: cr.color_btn,
+            didOpen:()=>{
+                $.getJSON('https://raw.githubusercontent.com/kurotsmile/Database-Store-Json/main/lang.json', function(data) {
+                    $.each(data.all_item,function(index,lang){
+                        if(lang.key==m.lang)
+                            $("#dropdown_lang").append($('<option>', { value: lang.key,text : lang.name,selected:true}));
+                        else
+                            $("#dropdown_lang").append($('<option>', { value: lang.key,text : lang.name}));
+                    });
+                });
+            }
+        }).then((result)=>{
+            if(result.isConfirmed){
+                cr.lang=$("#dropdown_lang").val();
+                localStorage.setItem("lang",cr.lang);
+                if(act_done!=null) act_done();
+            }
+        });
+    }
+
+    show_contact(){
+
+    }
+}
+var cr=new Carrot();
