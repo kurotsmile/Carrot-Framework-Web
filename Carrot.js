0fbee1afe745cfb16a1bbf60f017d4ead3abd078
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-09 14:43:16 +0700

    update payment
    update payment

--- Carrot.js
+++ Carrot.js
@@ -74,5 +74,24 @@ class Carrot{
             confirmButtonColor: cr.color_btn
         })
     }
+
+    show_pay(){
+        Swal.fire({
+            title: 'Order Item',
+            html: `
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_xclick">
+                <input type="hidden" name="business" value="tranthienthanh93@gmail.com">
+                <input type="hidden" name="item_name" value="Sample Item">
+                <input type="hidden" name="amount" value="2.00">
+                <input type="hidden" name="currency_code" value="USD">
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+              </form>
+            `,
+            showConfirmButton: false,
+            confirmButtonColor: cr.color_btn
+          });
+    }
 }
 var cr=new Carrot();
