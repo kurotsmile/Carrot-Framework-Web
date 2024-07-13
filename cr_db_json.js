46bfd7e8a85c28abe7154e0deddb4f1be55537e8
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-13 08:53:26 +0700

    Update db js

--- cr_db_json.js
+++ cr_db_json.js
@@ -1,12 +1,12 @@
 class Carrot_Database_Json{
 
-    edit(data){
+    edit(data,act_done){
         var html='';
         html+='<form class="text-left">';
         $.each(data,function(k,v){
             html+='<div class="form-group">';
             html+='<label for="data_field_'+k+'"><i class="fas fa-database"></i> '+k+'</label>';
-            html+='<input class="form-control" value="'+v+'"/>';
+            html+='<input class="form-control inp_db" db-key="'+k+'" value="'+v+'"/>';
             html+='</div>';
         });
         html+='</form>';
@@ -16,6 +16,16 @@ class Carrot_Database_Json{
             showCancelButton: true,
             showCloseButton: true,
             confirmButtonColor: cr.color_btn
+        }).then((result)=>{
+            if(result.isConfirmed){
+                var db={};
+                $(".inp_db").each(function(index,emp){
+                    var db_key=$(emp).attr("db-key");
+                    var db_val=$(emp).val();
+                    db[db_key]=db_val;
+                });
+                if(act_done!=null) act_done(db);
+            }
         });
     }
 }
