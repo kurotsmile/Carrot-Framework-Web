5f809acf0de0f11f460cfc0b126bea1665dcbbc8
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-14 16:59:59 +0700

    add icon for cr_framwork

--- cr_db_json.js
+++ cr_db_json.js
@@ -17,8 +17,18 @@ class Carrot_Database_Json{
         html+='<form class="text-left">';
         $.each(data,function(k,v){
             html+='<div class="form-group">';
-            html+='<label for="data_field_'+k+'"><i class="fas fa-database"></i> '+k+'</label>';
-            html+='<input class="form-control inp_db" db-key="'+k+'" value="'+v+'"/>';
+                html+='<label>'+k+'</label>';
+                html+='<div class="input-group">';
+                    html+='<div class="input-group-prepend">';
+                        html+='<span class="input-group-text">'+cr_data.getIconBykey(k)+'</span>';
+                    html+='</div>';
+                    
+                    html+='<input class="form-control inp_db" db-key="'+k+'" value="'+v+'" id="inp_db_'+k+'"/>';
+                    html+='<div class="input-group-append">';
+                        html+='<span role="button" class="input-group-text" onClick="cr.copy(\'#inp_db_'+k+'\');"><i class="fas fa-copy"></i></span>';
+                        html+='<span role="button" class="input-group-text" onClick="cr.paste(\'#inp_db_'+k+'\');"><i class="fas fa-paste"></i></span>';
+                    html+='</div>';
+                html+='</div>';
             html+='</div>';
         });
         html+='</form>';
@@ -44,11 +54,21 @@ class Carrot_Database_Json{
     getIconBykey(key){
         var icon='';
         switch (key) {
-            case 'name':
-                
+            case 'lang':
+                icon='<i class="fas fa-globe"></i>';
                 break;
-        
+            case 'id':
+                icon='<i class="fas fa-robot"></i>';
+                break;
+            case 'name':
+                icon='<i class="fas fa-file-signature"></i>';
+                break;
+            case 'date':
+            case 'publishedAt':
+                icon='<i class="fas fa-calendar"></i>';
+                break;
             default:
+                icon='<i class="fas fa-database"></i>';
                 break;
         }
         return icon;
