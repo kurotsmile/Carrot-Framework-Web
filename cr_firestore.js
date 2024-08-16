2a95c9a42f5ae87815457d943b87bdf83752a1d1
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-08-16 09:34:02 +0700

    Update frise

--- cr_firestore.js
+++ cr_firestore.js
@@ -2,7 +2,7 @@ class CR_FireStore{
     id_project="";
     api_key="";
 
-    add(data_document,collection){
+    add(data_document,collection,act_done=null,act_fail=null){
         var firestoreUrl = 'https://firestore.googleapis.com/v1/projects/'+cr_firestore.id_project+'/databases/(default)/documents/'+collection+'/?key='+cr_firestore.api_key;
             var data={};
             data["fields"]=cr_firestore.convertToFirestoreFormat(data_document);
@@ -10,17 +10,93 @@ class CR_FireStore{
                 url: firestoreUrl,
                 method: 'POST',
                 headers: {
-                    
                     'Content-Type': 'application/json'
                 },
                 data: JSON.stringify(data),
                 success: function(response) {
+                    if(act_done)act_done();
                     console.log('Document added successfully:', response);
                 },
                 error: function(error) {
+                    if(act_fail) act_fail();
                     console.error('Error adding document:', error);
                 }
             });
+    }
+
+    get(collection,document,act_done=null,act_fail=null){
+        $.ajax({
+            url:'https://firestore.googleapis.com/v1/projects/'+cr_firestore.id_project+'/databases/(default)/documents/'+collection+'/'+document+'/?key='+cr_firestore.api_key,
+            type: 'GET',
+            success: function(response) {
+                if(act_done) act_done(cr_firestore.convertFromFirestoreFormat(response.fields));
+                console.log("Dữ liệu document từ Firestore:", response);
+            },
+            error: function(error) {
+                if(act_fail) act_fail();
+                console.error("Lỗi khi lấy document:", error);
+            }
+        });
+    }
+
+    delete(collection,document,act_done=null,act_fail=null){
+        $.ajax({
+            url:'https://firestore.googleapis.com/v1/projects/'+cr_firestore.id_project+'/databases/(default)/documents/'+collection+'/'+document+'/?key='+cr_firestore.api_key,
+            type: 'DELETE',
+            success: function(response) {
+                if(act_done) act_done();
+                console.log("Document đã bị xóa thành công:", response);
+            },
+            error: function(error) {
+                if(act_fail) act_fail();
+                console.error("Lỗi khi xóa document:", error);
+            }
+        });
+    }
+
+    set(data_document,collection,document,act_done=null,act_fail=null){
+        this.update(data_document,collection,document,act_done,act_fail);
+    }
+
+    update(data_document,collection,document,act_done=null,act_fail=null){
+        var data={};
+        data["fields"]=cr_firestore.convertToFirestoreFormat(data_document);
+        $.ajax({
+            url: 'https://firestore.googleapis.com/v1/projects/'+cr_firestore.id_project+'/databases/(default)/documents/'+collection+'/'+document+'/?key='+cr_firestore.api_key,
+            type: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(data),
+            success: function(response) {
+                if(act_done) act_done();
+                console.log("Document đã được cập nhật thành công:", response);
+            },
+            error: function(error) {
+                if(act_fail) act_fail();
+                console.error("Lỗi khi cập nhật document:", error);
+            }
+        });
+    }
+    
+    list(collection,act_done=null,act_fail=null){
+        $.ajax({
+            url: 'https://firestore.googleapis.com/v1/projects/'+cr_firestore.id_project+'/databases/(default)/documents/'+collection+'/?key='+cr_firestore.api_key,
+            type: 'GET',
+            success: function(data) {
+                var list=[];
+                for(var i=0;i<data.documents.length;i++){
+                    var obj_data=cr_firestore.convertFromFirestoreFormat(data.documents[i].fields);
+                    obj_data["id_doc"]=data.documents[i].name.split("/").pop();
+                    list.push(obj_data);
+                }
+                if(act_done) act_done(list);
+            },
+            error: function(error) {
+                console.error("Lỗi khi lấy dữ liệu:", error);
+                if(act_fail) act_fail();
+            }
+        });
     }
 
     convertToFirestoreFormat(data) {
@@ -95,25 +171,6 @@ class CR_FireStore{
         return data;
     }
 
-    list(collection,act_done=null,act_fail=null){
-        $.ajax({
-            url: 'https://firestore.googleapis.com/v1/projects/'+cr_firestore.id_project+'/databases/(default)/documents/'+collection+'/?key='+cr_firestore.api_key,
-            type: 'GET',
-            success: function(data) {
-                var list=[];
-                for(var i=0;i<data.documents.length;i++){
-                    var obj_data=cr_firestore.convertFromFirestoreFormat(data.documents[i].fields);
-                    obj_data["id_doc"]=data.documents[i].name.split("/").pop();
-                    list.push(obj_data);
-                }
-                if(act_done) act_done(list);
-            },
-            error: function(error) {
-                console.error("Lỗi khi lấy dữ liệu:", error);
-                if(act_fail) act_fail();
-            }
-        });
-    }
 }
 
 var cr_firestore=new CR_FireStore();
