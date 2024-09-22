8ea63012de4b8ac52ef34e455ace330d87ef9e9a
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-09-23 03:20:37 +0700

    Update icon for cms

--- cr_icon.js
+++ cr_icon.js
@@ -19,13 +19,34 @@ class CR_Icons{
         'fas fa-weight-hanging',
         'fas fa-weight',
         'fas fa-warehouse',
-        'fas fa-wallet'
+        'fas fa-wallet',
+        'fas fa-walking',
+        'fas fa-volume-up',
+        'fas fa-volume-off',
+        'fas fa-volume-mute',
+        'fas fa-volume-down',
+        'fas fa-volleyball-ball',
+        'fas fa-vihara',
+        'fas fa-video-slash',
+        'fas fa-video',
+        'fas fa-vials',
+        'fas fa-vial',
+        'fas fa-venus-mars',
+        'fas fa-venus-double',
+        'fas fa-venus',
+        'fas fa-vector-square',
+        'fas fa-utensils',
+        'fas fa-utensil-spoon',
+        'fas fa-store',
+        'fas fa-phone',
+        'fas fa-map-marker-alt',
+        'fas fa-envelope'
     ]
 
     show_select(emp_inp=null){
         cr.msg('<div id="cr_all_item_icon"></div>',"Select Icon","",()=>{
             $.each(cr_icon.list_icon,function(index,ico){
-                var emp_icon=$(`<button class="btn btn-sm btn-light m-2"><i class="${ico}"></i></button>`)
+                var emp_icon=$(`<button class="btn btn-sm btn-light m-1"><i class="${ico}"></i></button>`)
                 $(emp_icon).click(function(){
                     $("#"+emp_inp).val(ico);
                     Swal.close();
