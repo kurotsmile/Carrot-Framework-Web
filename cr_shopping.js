78288a20fb7d1d392db3344075fa60f16768a097
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-09-26 13:31:47 +0700

    Add checkout

--- cr_shopping.js
+++ cr_shopping.js
@@ -1,13 +1,22 @@
-//Front End
+//Front End - Shppping
+//Required *carrot.js
+
 class Carrot_Shopping{
     paypal_api_key="";
     paypal_api_scenrest="";
     cart_data=[];
     emp_cart_menu=null;
+    shipping_price="1";
+    tax_price="1"
 
-    onLoad(id_emp_cart_menu=null){
+    Total_price=0;
+
+    onLoad(id_emp_cart_menu=null,api_key="",api_scenrest=""){
+        cr_shopping.paypal_api_key=api_key;
+        cr_shopping.paypal_api_scenrest=api_scenrest;
         if(localStorage.getItem("cart_data")) cr_shopping.cart_data=JSON.parse(localStorage.getItem("cart_data"));
         if(id_emp_cart_menu) cr_shopping.emp_cart_menu=id_emp_cart_menu;
+        if(cr_shopping.paypal_api_key!="") $('head').append('<script src="https://www.paypal.com/sdk/js?client-id='+cr_shopping.paypal_api_key+'&intent=authorize"><\/script>');
     }
 
     cart_item(data){
@@ -55,13 +64,13 @@ class Carrot_Shopping{
             }
         html+='</div>';
         var emp_page_cart=$(html);
-        var Total_price=0;
+        cr_shopping.Total_price=0;
         $.each(cr_shopping.cart_data,function(index,c_item){
-            Total_price+=parseFloat(c_item.price);
+            cr_shopping.Total_price+=parseFloat(c_item.price);
             $(emp_page_cart).find("#cr_list_all_item_cart").append(cr_shopping.cart_item(c_item));
         });
 
-        $(emp_page_cart).find("#cr_cart_total_price").html(Total_price.toFixed(2));
+        $(emp_page_cart).find("#cr_cart_total_price").html(cr_shopping.Total_price.toFixed(2));
 
         $(emp_page_cart).find("#btn_shoping_delete_all").click(()=>{
             cr.msg_question("Are you sure you want to delete all items in your cart?","Shopping cart",()=>{
@@ -73,6 +82,58 @@ class Carrot_Shopping{
             if(act_checkout) act_checkout();
         });
         return emp_page_cart;
+    }
+
+    page_checkout(content){
+        var html_page=$(`
+            <div class="row mt-5 mb-5">
+            <div class="col-md-4 order-md-2 mb-4">
+              <h4 class="d-flex justify-content-between align-items-center mb-3">
+                <span class="text-muted">Your cart</span>
+                <span class="badge badge-secondary badge-pill">3</span>
+              </h4>
+
+              <ul class="list-group mb-3" id="cartallitems"></ul>
+
+              <div id="paypal-button-container" class="my-3 w-100">
+                <div class="w-100 bg-light rounded p-3"><i class="fas fa-spinner fa-spin"></i> Loading...</div>
+              </div>
+            </div>
+            <div class="col-md-8 order-md-1">${content}</div>
+          </div>
+        `);
+        
+        cr_shopping.Total_price=0;
+        $.each(cr_shopping.cart_data,function(index,c){
+            cr_shopping.Total_price+=parseFloat(c.price);
+            $(html_page).find("#cartallitems").append(cr_shopping.checkout_cart_item(c));
+        });
+        
+        $(html_page).find("#cartallitems").append(cr_shopping.checkout_cart_info("Total",cr_shopping.Total_price));
+        return html_page;
+    }
+
+    checkout_cart_item(data){
+        var html=$(`
+                <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 class="my-0">${data.name}</h6>
+                    <small class="text-muted text-truncate">1 Product</small>
+                  </div>
+                  <span class="text-muted">$${parseFloat(data.price).toFixed(2)}</span>
+                </li>
+            `);
+        return html;
+    }
+
+    checkout_cart_info(label,price){
+        var html=$(`
+            <li class="list-group-item d-flex justify-content-between bg-light">
+                <span>${label}</span>
+                <strong>$${price}</strong>
+            </li>
+        `);
+      return html;
     }
 
     update_cart(){
@@ -130,6 +191,92 @@ class Carrot_Shopping{
         var html='';
         html+='<div class="col-12 text-center"><div class="bg-light p-5 rounded"><b><i class="fas fa-sad-tear fa-lg"></i></b><br/>Cart is empty!</div></div>';
         return html;
+    }
+
+    create_btn_checkout(id_emp_btn_paypal="#paypal-button-container"){
+        setTimeout(()=>{
+            $(id_emp_btn_paypal).empty();
+              paypal.Buttons({
+                createOrder: function (data, actions) {
+                  var allFilled = true;
+                  w.error_pay=false;
+
+                  $('input[required]').each(function() {
+                    if ($(this).val() === '') {
+                      allFilled = false;
+                      w.error_pay=true;
+                      $(this).css('border', '2px solid red');
+                    } else {
+                      $(this).css('border', '');
+                    }
+                  });
+    
+                  if(allFilled==false){
+                    cr.msg("Please fill in all information!","Missing data","warning")
+                    return true;
+                  }else{
+                    let data_bill={};
+                    $('input').each(function() {
+                        var id_field=$(this).attr("id");
+                        data_bill[id_field]=$(this).val();
+                    });
+                    data_bill["date"]=new Date().toLocaleString();
+                    w.data_bill=data_bill;
+                    localStorage.setItem("data_bill",JSON.stringify(data_bill));
+                  }
+                  // Lấy giá trị sản phẩm, phí vận chuyển và thuế từ DOM hoặc tính toán chúng
+                  var itemTotal = parseFloat($('#tt_price').html())||1; // Giá trị của sản phẩm
+                  var shippingCost = parseFloat(cr_shopping.shipping_price);
+                  var taxAmount = parseFloat(cr_shopping.tax_price);
+    
+                  return actions.order.create({
+                    purchase_units: [{
+                      amount: {
+                        currency_code: "USD",
+                        value: (itemTotal + shippingCost + taxAmount).toFixed(2), // Tổng giá trị đơn hàng bao gồm phí vận chuyển và thuế
+                        breakdown: {
+                          item_total: {
+                            currency_code: "USD",
+                            value: itemTotal.toFixed(2) // Giá trị của sản phẩm
+                          },
+                          shipping: {
+                            currency_code: "USD",
+                            value: shippingCost.toFixed(2) // Phí vận chuyển
+                          },
+                          tax_total: {
+                            currency_code: "USD",
+                            value: taxAmount.toFixed(2) // Thuế
+                          }
+                        }
+                      }
+                    }]
+                  });
+                },
+                onApprove: function (data, actions) {
+                  return actions.order.authorize().then(function (authorization) {
+                    var authorizationID = authorization.purchase_units[0].payments.authorizations[0].id;
+                    window.location.href = "index.html?p=done&authorization_id=" + authorizationID;
+                  });
+                },
+                onCancel: function (data) {
+                  Swal.fire({
+                    icon: 'info',
+                    title: 'Payment Cancelled',
+                    text: 'You have cancelled the payment.',
+                  });
+                },
+                onError: function (err) {
+                  console.log(err);
+                  if(w.error_pay==false){
+                    Swal.fire({
+                      icon: 'error',
+                      title: 'Payment Error',
+                      text: 'There was an issue with your payment. Please try again.',
+                    });
+                  }
+                }
+              }).render(id_emp_btn_paypal);
+        },1000);
     }
 }
 
